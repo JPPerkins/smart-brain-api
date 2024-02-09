d3248901b795e5762c0d28a9e58c9f74bc328a4e
JPPerkins <jcperkins482@gmail.com>
2024-02-09 14:17:54 -0500

    fixing module issue

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import knex from 'knex';
 import handleRegister from './controllers/register.js';
 import handleSignIn from './controllers/signin.js';
 import handleProfileGet from './controllers/profile.js';
-import { handleImage, handleApiCall } from './controllers/image';
+import { handleImage, handleApiCall } from './controllers/image.js';
 
 const db = knex({
 	client: 'pg',
